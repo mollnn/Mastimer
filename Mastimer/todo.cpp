03726f7da5607251ab99a9c5c841fe2a86d18f9e
mollnn <1084918073@qq.com>
2021-05-26 16:13:38 +0800

    dev branch initial commit

---
+++ Mastimer/todo.cpp
@@ -0,0 +1,6 @@
+#include "todo.h"
+
+Todo::Todo()
+{
+
+}
---
+++ Mastimer/todolist.h
@@ -0,0 +1,13 @@
+#ifndef TODOLIST_H
+#define TODOLIST_H
+
+#include <QObject>
+
+class Todolist
+{
+    Q_OBJECT
+public:
+    Todolist();
+};
+
+#endif // TODOLIST_H
